53b942d489e812369e8d45d99726fed07b072a09
Shadow <your.email@example.com>
2025-02-21 16:07:00 +0530

    Fixed Server Errors

--- backend/db.js
+++ backend/db.js
@@ -2,9 +2,7 @@ const { MongoClient } = require('mongodb');
 require('dotenv').config();
 
 const uri = process.env.MONGODB_URI;
-console.log('MONGODB_URI:', uri); // Add this line to check the URI
-
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+const client = new MongoClient(uri);
 
 const connectDB = async () => {
   try {
